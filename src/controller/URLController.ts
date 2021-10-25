f0b6dce484476f2a32f4d9d5eec6b53048dd96cc
NGustavo011 <gsn181200@gmail.com>
2021-10-25 14:40:10 -0300

    URL control file termination

--- src/controller/URLController.ts
+++ src/controller/URLController.ts
@@ -1,3 +1,4 @@
+import { URLModel } from "../database/model/URL";
 import { Request, Response } from "express";
 import shortId from 'shortid';
 import { API_URL } from "../config/Constants";
@@ -5,24 +6,32 @@ import { API_URL } from "../config/Constants";
 export class URLController {
     public async shorten(req: Request, res: Response): Promise<void>{
         // Verificar se a URL já não existe
-        // Criar o hash para a URL
         const { originURL } = req.body;
+        const url = await URLModel.findOne({ originURL });
+        if(url){
+            res.json(url);
+            return;
+        }
+        // Criar o hash para a URL
         const hash = shortId.generate();
         const shortURL = `${API_URL}/${hash}`;
         // Salvar a URL no banco
+        const newURL = await URLModel.create({ originURL, hash, shortURL })
         // Retornar a URL salva 
-        res.json({ originURL, hash, shortURL });
+        res.json({ newURL });
     }
+
     public async redirect(req: Request, res: Response): Promise<void>{
         // Pega hash da URL
         const { hash } = req.params;
+        const url = await URLModel.findOne( {hash} );
+
         // Encontrar a URL original pelo hash
-        const url = {
-            originURL: "https://www.kabum.com.br/produto/102002/headset-sem-fio-gamer-hyperx-cloud-flight-drivers-50mm-led-multiplas-plataformas-hx-hscf-bk-am",
-            hash: "R2Qak9oVx",
-            shortURL: "http://localhost:3333/R2Qak9oVx"
+        if(url){
+            // Redirecionar para a URL original com o que encontramos no DB
+            res.redirect(url.originURL);
         }
-        // Redirecionar para a URL original com o que encontramos no DB
-        res.redirect(url.originURL);
+
+        res.status(400).json({ error: "URL not found" });
     }
 }
